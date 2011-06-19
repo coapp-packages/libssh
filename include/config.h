6745e8988f5969c6e7947842fd1bfae02cd08f58
HamishC <hamish.cuimming@gmail.com>
2011-06-19 20:50:01 +1200

    Added .buildinfo and vstudio project

---
+++ include/config.h
@@ -0,0 +1,112 @@
+
+/************************** HEADER FILES *************************/
+
+/* Define to 1 if you have the <argp.h> header file. */
+//#define HAVE_ARGP_H 1
+
+/* Define to 1 if you have the <pty.h> header file. */
+//#define HAVE_PTY_H 1
+
+/* Define to 1 if you have the <termios.h> header file. */
+//#define HAVE_TERMIOS_H 1
+
+/* Define to 1 if you have the <openssl/aes.h> header file. */
+//#define HAVE_OPENSSL_AES_H 1
+
+/* Define to 1 if you have the <wspiapi.h> header file. */
+//#define HAVE_WSPIAPI_H 1
+
+/* Define to 1 if you have the <openssl/blowfish.h> header file. */
+//#define HAVE_OPENSSL_BLOWFISH_H 1
+
+/* Define to 1 if you have the <openssl/des.h> header file. */
+//#define HAVE_OPENSSL_DES_H 1
+
+/* Define to 1 if you have the <pthread.h> header file. */
+//#define HAVE_PTHREAD_H 1
+
+
+/*************************** FUNCTIONS ***************************/
+
+/* Define to 1 if you have the `snprintf' function. */
+//#define HAVE_SNPRINTF 1
+
+/* Define to 1 if you have the `_snprintf' function. */
+//#define HAVE__SNPRINTF 1
+
+/* Define to 1 if you have the `_snprintf_s' function. */
+#define HAVE__SNPRINTF_S 1
+
+/* Define to 1 if you have the `vsnprintf' function. */
+//#define HAVE_VSNPRINTF 1
+
+/* Define to 1 if you have the `_vsnprintf' function. */
+//#define HAVE__VSNPRINTF 1
+
+/* Define to 1 if you have the `_vsnprintf_s' function. */
+#define HAVE__VSNPRINTF_S 1
+
+/* Define to 1 if you have the `strncpy' function. */
+#define HAVE_STRNCPY 1
+
+/* Define to 1 if you have the `cfmakeraw' function. */
+//#define HAVE_CFMAKERAW 1
+
+/* Define to 1 if you have the `getaddrinfo' function. */
+#define HAVE_GETADDRINFO 1
+
+/* Define to 1 if you have the `poll' function. */
+//#define HAVE_POLL 1
+
+/* Define to 1 if you have the `select' function. */
+//#define HAVE_SELECT 1
+
+/* Define to 1 if you have the `regcomp' function. */
+//#define HAVE_REGCOMP 1
+
+/* Define to 1 if you have the `clock_gettime' function. */
+//#define HAVE_CLOCK_GETTIME 1
+
+/*************************** LIBRARIES ***************************/
+
+/* Define to 1 if you have the `crypto' library (-lcrypto). */
+#define HAVE_LIBCRYPTO 1
+
+/* Define to 1 if you have the `gcrypt' library (-lgcrypt). */
+//#define HAVE_LIBGCRYPT 1
+
+/* Define to 1 if you have the `z' library (-lz). */
+//#define HAVE_LIBZ 1
+
+/* Define to 1 if you have the `pthread' library (-lpthread). */
+//#define HAVE_PTHREAD 1
+
+
+/**************************** OPTIONS ****************************/
+
+/* Define to 1 if you want to enable ZLIB */
+//#define WITH_LIBZ 1
+
+/* Define to 1 if you want to enable SFTP */
+#define WITH_SFTP 1
+
+/* Define to 1 if you want to enable SSH1 */
+#define WITH_SSH1 1
+
+/* Define to 1 if you want to enable server support */
+#define WITH_SERVER 1
+
+/* Define to 1 if you want to enable debug output for crypto functions */
+//#define DEBUG_CRYPTO 1
+
+/* Define to 1 if you want to enable pcap output support (experimental) */
+//#define WITH_PCAP 1
+
+/* Define to 1 if you want to enable calltrace debug output */
+//#define DEBUG_CALLTRACE 1
+
+/*************************** ENDIAN *****************************/
+
+/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
+   significant byte first (like Motorola and SPARC, unlike Intel). */
+//#define WORDS_BIGENDIAN 1
